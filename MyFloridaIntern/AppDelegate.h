16d4e7493193da59bb07560fbf4d7d6745ed9407
Brandon Cordell <brandon@brandoncordell.com>
2017-01-12 12:32:30 -0500

    Initial Commit

---
+++ MyFloridaIntern/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFloridaIntern
+//
+//  Created by Brandon Cordell on 1/12/17.
+//  Copyright © 2017 Complete Technology Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFloridaIntern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFloridaIntern
+//
+//  Created by Brandon Cordell on 1/12/17.
+//  Copyright © 2017 Complete Technology Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
