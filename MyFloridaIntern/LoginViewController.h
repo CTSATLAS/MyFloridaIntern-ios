23b64693ae0c3fa0ca234d109de0363efde61d21
Brandon Cordell <brandon@brandoncordell.com>
2017-01-13 12:26:54 -0500

    Add login background image

--- MyFloridaIntern/LoginViewController.h
+++ MyFloridaIntern/LoginViewController.h
@@ -1,13 +1,13 @@
 //
-//  UIViewController_LoginViewController.h
+//  LoginViewController.h
 //  MyFloridaIntern
 //
 //  Created by Brandon Cordell on 1/12/17.
 //  Copyright © 2017 Complete Technology Solutions. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#ifndef LoginViewController_h
+#define LoginViewController_h
 
-@interface UIViewController ()
 
-@end
+#endif /* LoginViewController_h */
---
+++ MyFloridaIntern/LoginViewController.m
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.m
+//  MyFloridaIntern
+//
+//  Created by Brandon Cordell on 1/12/17.
+//  Copyright © 2017 Complete Technology Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
